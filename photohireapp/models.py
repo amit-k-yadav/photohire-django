ade3f4bb74a28f441d78aae168896fb6816ea617
Amit Yadav <amityadavk2@gmail.com>
2020-02-26 02:47:19 +0530

    ADD - A simple way to add images

--- photohireapp/models.py
+++ photohireapp/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+class Hotel(models.Model): 
+    name = models.CharField(max_length=50) 
+    hotel_Main_Img = models.ImageField(upload_to='images/')
