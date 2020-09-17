a8940344b9c6f14fb5086503e9f6b98fc0b778a5
Amit Yadav <amityadavk2@gmail.com>
2020-09-17 19:02:54 +0530

    ADD - Forgot Password feature

--- photohire/settings.py
+++ photohire/settings.py
@@ -126,3 +126,10 @@ STATICFILES_DIRS = [
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'media') 
 
 MEDIA_URL = '/media/'
+
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'youremail@gmail.com'
+EMAIL_HOST_PASSWORD = 'yourPassword'
