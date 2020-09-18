a40ab9238e73324d2f8e6abb8092296d3c75d173
Amit Yadav <amityadavk2@gmail.com>
2020-09-18 18:05:41 +0530

    ADD - More comments

--- photohire/urls.py
+++ photohire/urls.py
@@ -33,7 +33,11 @@ urlpatterns = [
     # Hire a photographer with id=pg_id
     path('hire/<int:pg_id>', hire, name="hire"),
 
-    # Forget Password
+
+    ############## Start : Password reset URLs ##############
+
+    # Forget Password - Renders "password_reset_form.html"
+    # "auth_views.PasswordResetView.as_view" view below is a library and it's executed by django
     path('password-reset/',
         auth_views.PasswordResetView.as_view(
              template_name='registrations/password_reset_form.html',
@@ -41,22 +45,37 @@ urlpatterns = [
              email_template_name='registrations/password_reset_email.html'
          ),
          name='password_reset'),
+
+
+    # User get redirected here after entering his/her email - Renders "password_reset_form.html"
+    # "auth_views.PasswordResetDoneView.as_view" view below is a library and it's executed by django
     path('password-reset/done/',
          auth_views.PasswordResetDoneView.as_view(
              template_name='registrations/password_reset_done.html'
          ),
          name='password_reset_done'),
+
+
+    # User comes here when he/she clicks the link in the email - Renders "password_reset_confirm.html"
     path('password-reset-confirm/<uidb64>/<token>/',
          auth_views.PasswordResetConfirmView.as_view(
              template_name='registrations/password_reset_confirm.html'
          ),
          name='password_reset_confirm'),
+
+
+    # This is for information after user has changed the password
     path('password-reset-complete/',
          auth_views.PasswordResetCompleteView.as_view(
              template_name='registrations/password_reset_complete.html'
          ),
          name='password_reset_complete'),
+    
+    ############## END : Password reset URLs ##############
 ]
 
+# This is additional URL used for Static content (i.e CSS and JavaScript etc.)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+# This is additional URL used for Media content (i.e images, profile pictures etc.)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
